2ab4b99382470a17e6c3b9896dd5545789792515
dhiennyam13 <154111666+dhiennyam13@users.noreply.github.com>
2024-02-15 14:11:08 +0700

    Add files via upload

---
+++ Test_Karawaci_Algoritma2/Test_Karawaci_Algoritma2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input1 = { 1, 2, 3, 4, 5 };
+        int[] input2 = { 15, 25, 35 };
+        int[] input3 = { 8, 8 };
+
+        int output1 = CalculateSum(input1);
+        int output2 = CalculateSum(input2);
+        int output3 = CalculateSum(input3);
+
+        Console.WriteLine($"Output 1: {output1}");
+        Console.WriteLine($"Output 2: {output2}");
+        Console.WriteLine($"Output 3: {output3}");
+    }
+
+    static int CalculateSum(int[] numbers)
+    {
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            if (num % 10 == 5)
+            {
+                sum += 2;
+            }
+            else
+            {
+                sum += 1;
+            }
+        }
+        return sum;
+    }
+}
